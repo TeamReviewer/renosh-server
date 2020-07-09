a3bc3f6f66ff0464505454d73a6acb88064ff013
code4human <tena.kim10@gmail.com>
2020-07-09 17:39:55 +0900

    USER API 작성

---
+++ routes/User_cosmos.js
@@ -0,0 +1,78 @@
+require('dotenv').config()
+const { CosmosClient } = require("@azure/cosmos");
+
+// connect cosmos DB
+const endpoint = process.env.COSMOSDB_ENDPOINT;
+const key = process.env.COSMOSDB_KEY; 
+// const endpoint = "https://renosh.documents.azure.com:443/";
+// const key = "VGGuRGqhhxhHNuDbdpV0ypJo3DlGRtchkGH1ofFgaq3XFX4ynqcJlWbVVoRMk48Gn8jQjxHjfFKpcvbTarRd4A=="; 
+const client = new CosmosClient({ endpoint, key });
+const database = client.database('renosh');
+const container = database.container('user');
+
+
+async function getListOfUsers(req, res){
+    try{
+        const { resources: UserList } = await container.items.readAll().fetchAll();
+        res.status(200).json(UserList);
+        UserList.forEach(item => {
+            console.log(`${item.id} - ${item.name}`);
+        });
+    }catch(error){
+        res.status(500).send(error);
+    }
+}
+
+
+async function postUserInfo(req, res){
+    const userinfo = {
+        user_id: req.body.user_id,
+        name: req.body.name,
+        register_type: req.body.register_type
+    };
+    try{
+        const { resource } = await container.items.create(userinfo);
+        res.status(200).json({"user_id" : resource.id});
+        console.log(`User ${user_id} created successfully`);
+    }catch(error){
+        res.status(500).send(error);
+    }
+}
+
+
+async function getUserById(req, res){
+    try{
+        const item = container.item(req.params.user_id, undefined);
+        const {resource: user} = await item.read();
+        res.status(200).json(user);
+        console.log(`${user}`);
+    }catch(error){
+        res.status(500).send(error);
+    }
+}
+
+
+async function updateUserById(req, res){
+    // pass
+}
+
+
+async function deleteUserById(req, res){
+    const user_id = req.params.user_id;
+    try{
+        const {resource: item} = await container.item(user_id, undefined).delete();
+        res.status(200).json({"user_id":user_id});
+        console.log(`User ${user_id} deleted successfully`);
+    } catch(error){
+        res.status(500).send(error);
+    }
+}
+
+
+module.exports = {
+    getListOfUsers,
+    postUserInfo,
+    getUserById,
+    updateUserById,
+    deleteUserById
+}
