6652be0d220fbac12d757fb9c9a794ded339337e
code4human <tena.kim10@gmail.com>
2020-07-16 05:49:38 +0900

    swagger 설정

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 // const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express(); 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+  info: {
+    // API informations (required)
+    title: 'Renosh API', // Title (required)
+    version: '1.0.0', // Version (required) of Team's Docs, not module's
+    description: 'Renosh API Information', // Description (optional)
+    contact: {
+      name: 'Hanium'
+    },
+    servers: ['52.231.64.216:5000', 'localhost:5000']
+  },
+  host: '52.231.64.216:5000', // Host (optional)
+  basePath: '/', // Base path (optional)
+}
+
+// Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // Path to the API docs
+  apis: ['./docs/index.js'], // ./routes/*.js
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+var swaggerSpec = swaggerJsDoc(options)
 
 //json parser
 app.use(bodyParser.urlencoded({extended:false}));
@@ -18,6 +45,7 @@ const HighRouter = require('./routes/Highlight');
 app.use('/api/users', UserRouter);
 app.use('/api/books', BookRouter);
 app.use('/api/highlights', HighRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // //connect cosmos DB
 // const endpoint = process.env.COSMOSDB_ENDPOINT; // Add your endpoint
