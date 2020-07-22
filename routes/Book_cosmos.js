5212eeeb92273d2fe2ca83c13dfcd5a7b5f43f7c
SeongMinKwon <sleepingsm813@gmail.com>
2020-07-22 21:23:48 +0900

    Test post container with partition key

--- routes/Book_cosmos.js
+++ routes/Book_cosmos.js
@@ -2,12 +2,11 @@
 const client = require('./config');
 
 const database = client.database('renosh');
-const container = database.container('book' );
+const container = database.container('post' );
 
 async function getListOfBooks(req, res){
     try{
         const { resources: bookList } = await container.items.readAll().fetchAll();
-        // console.log(bookList)
         res.json(bookList);
     }catch(error){
         res.status(500).send(error);
@@ -17,7 +16,7 @@ async function getListOfBooks(req, res){
 async function getBookWithId(req, res){
     try{
         const bookid = req.params.bookid;
-        const { resource: book } = await container.item(bookid, undefined).read();
+        const { resource: book } = await container.item(bookid, bookid).read();
         console.log(book);
         res.json(book);
     }catch(error){
@@ -27,14 +26,6 @@ async function getBookWithId(req, res){
 
 async function postBookInfo(req, res){
     const bookinfo = req.body;
-    // {
-    //     title: "The Little Prince",
-    //     author: "Antoine de Saint-Exupéry",
-    //     summary: "Summary of The Little Prince",
-    //     image: "https://images-na.ssl-images-amazon.com/images/I/41MkVPBdOOL._SX317_BO1,204,203,200_.jpg",
-    //     epubURL: "https://pdfstop.com/get-download?file=838"
-    // }
-
     try{
         const { resource } = await container.items.create(bookinfo);
         console.log(resource.id);
@@ -47,16 +38,8 @@ async function postBookInfo(req, res){
 async function putBookInfo(req, res){
     const bookid = req.params.bookid;
     const bookinfo = req.body;
-    // {
-    //     id : bookid,
-    //     title: "The Little Prince",
-    //     author: "Antoine de Saint-Exupéry",
-    //     summary: "Summary of The Little Prince",
-    //     image: "https://images-na.ssl-images-amazon.com/images/I/41MkVPBdOOL._SX317_BO1,204,203,200_.jpg",
-    //     epubURL: "https://pdfstop.com/get-download?file=838"
-    // }
     try{
-        const { resource } = await container.item(bookid,undefined).replace(bookinfo);
+        const { resource } = await container.item(bookid,bookid).replace(bookinfo);
         res.send("Book info updated Succesfully");
     }catch(error){
         res.status(500).send(error);
@@ -66,7 +49,7 @@ async function putBookInfo(req, res){
 async function deleteBook(req, res){
     const bookid = req.params.bookid;
     try{
-        const {resource: item} = await container.item(bookid, undefined).delete();
+        const {resource: item} = await container.item(bookid, bookid).delete();
         res.status(200).json({"book id":bookid});
         console.log("Book deleted successfully");
     } catch(error){
